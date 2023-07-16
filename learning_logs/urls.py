50a38e3d5d86c33febeda208f0ece34c8e976c25
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-07-16 03:36:24 +0000

    adding pages for individual topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
 
     # Detail page fo a single topic
-    path('topics/<int:topic_id>/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
