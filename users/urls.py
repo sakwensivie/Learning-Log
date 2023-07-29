c8754022f1cf11c7ebf371e28d8a3bb197a5be56
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-07-29 10:54:44 +0000

    login working correctly

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,13 @@
 '''Defines URL patterns for users'''
 
 from django.urls import path
-from django.contrib.auth.views import login
+from django.contrib.auth.views import LoginView
 
 from . import views
 
+app_name = 'users'
 
 urlpatterns = [
     # Login page
-    path('login/', login, {'template_name': 'users/login.html'},
-        name='login'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
 ]
