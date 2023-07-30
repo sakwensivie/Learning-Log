87f5ecfc8b2fb44a1351a470c9cbfb907e71d773
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-07-30 10:31:35 +0000

    register, login, and logout working

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -89,24 +89,24 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_\
-            validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_\
-            validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_\
-            validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_\
-            validation.NumericPasswordValidator',
-    },
-]
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_\
+#             validation.MinimumLengthValidator',"OPTIONS": {
+#             "min_length": 9}
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_\
+#             validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_\
+#             validation.NumericPasswordValidator',
+#     },
+# ]
 
 
 # Internationalization
--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,10 @@ app_name = 'users'
 urlpatterns = [
     # Login page
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+
+    # Logout page
+    path('logout/', views.logout_view, name='logout'),
+
+    # Registeration page
+    path('register/', views.register, name='register'),
 ]
